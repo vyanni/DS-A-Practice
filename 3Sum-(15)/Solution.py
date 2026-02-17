cbac15f8bc8aaa04df29c9835fddd546a4e3fb99
vyanni <vianneysusanto@gmail.com>
2026-02-17 08:54:00 -0500

    Python solution for 15

--- 3Sum-(15)/Solution.py
+++ 3Sum-(15)/Solution.py
@@ -6,10 +6,40 @@ class Solution(object):
         """
         
         nums.sort()
-        fixedElement = nums[0]
+        tupleList = []
 
         for i in range(len(nums) - 2):
-            leftPointer = 1
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            leftPointer = i + 1
             rightPointer = len(nums) - 1
 
-            if(nums[leftPointer] + nums[rightPointer])
+            while leftPointer < rightPointer:
+                totalSum = nums[i] + nums[leftPointer] + nums[rightPointer]
+
+                if totalSum == 0:
+                    tupleList.append([nums[i], nums[leftPointer], nums[rightPointer]])
+
+                    while leftPointer < rightPointer and nums[leftPointer] == nums[leftPointer + 1]:
+                        leftPointer += 1
+                    while leftPointer < rightPointer and nums[rightPointer] == nums[rightPointer - 1]:
+                        rightPointer -= 1
+
+                    leftPointer += 1
+                    rightPointer -= 1
+                
+                elif totalSum < 0:
+                    leftPointer += 1
+                else:
+                    rightPointer -= 1
+        
+        return tupleList
+    
+#Runtime: 771ms 
+# {O(n^2) -> Takes O{n log n} to sort, then O(n) to fix each element, then O(n) to move the pointers per fixed element
+# This results in O(n^2) for the main look-through loop, and combined, its O(n logn + n^2), where n^2 grows faster so overall its
+# O(n^2)}
+#Memory Space: 18.98mb 
+#{For sorting, it takes O(log n) space, and returning there can be at most a possible of O(n^2) triplets}
